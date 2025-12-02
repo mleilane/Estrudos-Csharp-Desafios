9d0ac3187209d7172d526ca5b521230bf0ef4d8f
mleilane <leilane94silva@yahoo.com>
2025-12-02 20:50:20 -0300

    Desafio 03 - Conta Bancaria

---
+++ ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,18 @@
+﻿class ContaBancaria
+{
+    public string NumeroConta;
+    public double Saldo;
+
+    public ContaBancaria(string numeroConta, double saldo)
+    {
+        NumeroConta = numeroConta;
+        Saldo = saldo;
+    }
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+
+
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,9 @@
+﻿
+ContaBancaria conta1 = new ContaBancaria("1234-5",1000.00);
+
+Console.WriteLine($@"Conta: {conta1.NumeroConta}
+Saldo atual: {conta1.Saldo}");
+
+conta1.Depositar(500.00);
+Console.WriteLine(@$"Deposito realizado.
+Saldo atual: {conta1.Saldo}");
