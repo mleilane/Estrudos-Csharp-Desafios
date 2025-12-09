3b94226fc0f235e5a8e3150f2200f74ca2d82889
mleilane <leilane94silva@yahoo.com>
2025-12-09 14:30:06 -0300

    Adicionando o desafio 06: validação de acesso a filmes

---
+++ ValidacaoAcessoAFilmes/Filme.cs
@@ -0,0 +1,28 @@
+﻿class Filme
+{
+    public string Titulo;
+    public int ClassificacaoEtaria;
+
+    public Filme(string titulo, int classificacaoEtaria)
+    {
+        Titulo = titulo;
+        ClassificacaoEtaria = classificacaoEtaria;
+    }
+
+    public bool PodeAssistir(int idadeUsuario)
+    {
+        return idadeUsuario >= ClassificacaoEtaria;   
+    }
+
+    public string ExibirResultado (int idadeUsuario)
+    {
+        if(PodeAssistir (idadeUsuario))
+        {
+            return $"Usúario com {idadeUsuario} anos pode assistir ao filme {Titulo}";
+        }
+        else
+        {
+            return $"Usúario com {idadeUsuario} anos não pode assistir ao filme {Titulo}";
+        }
+    }
+}
---
+++ ValidacaoAcessoAFilmes/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Filme filme = new Filme("O Dilema das Redes",18);
+
+Console.WriteLine(filme.ExibirResultado(12));
+Console.WriteLine(filme.ExibirResultado(20));
+
