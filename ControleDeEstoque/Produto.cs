42a96afd47d72064259d3d091cdf92eca6d3e785
mleilane <leilane94silva@yahoo.com>
2025-12-11 20:25:09 -0300

    Adicionando o desafio de nº 07 - Controle de estoque

---
+++ ControleDeEstoque/Produto.cs
@@ -0,0 +1,31 @@
+﻿class Produto
+{
+    public string Nome;
+    private int QuantidadeEstoque;
+
+    public Produto(string nome, int quantidadeEstoque)
+    {
+        Nome = nome;
+        QuantidadeEstoque = quantidadeEstoque;
+    }
+    
+
+    public string Retirar(int quantidade)
+    {
+        if (quantidade <= QuantidadeEstoque)
+        {
+            QuantidadeEstoque -= quantidade;
+           return $"Retirada de {quantidade} unidades realizada com sucesso";
+        }
+        else
+        {
+            return $"Erro: A quantidade informada é superior ao estoque disponivel: {QuantidadeEstoque}";
+        }
+        
+    }
+
+    public string ExibirEstoque()
+    {
+        return $"Estoque: {QuantidadeEstoque}";
+    }
+}
---
+++ ControleDeEstoque/Program.cs
@@ -0,0 +1,9 @@
+﻿Produto produto01 = new Produto("Garrafa",15);
+
+Console.WriteLine(produto01.Retirar(5));
+Console.WriteLine(produto01.ExibirEstoque());
+
+Console.WriteLine(produto01.Retirar(30));
+
+
+
