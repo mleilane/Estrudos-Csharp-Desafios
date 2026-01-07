26d56699b4e8dfe083bdf5fca71163bbb287cf8c
mleilane <leilane94silva@yahoo.com>
2026-01-06 21:25:22 -0300

    desafio de nº 08: Sistema de pedidos para loja virtual

---
+++ SistemaDePedidosLojaVirtual/Pedido.cs
@@ -0,0 +1,23 @@
+﻿class Pedido
+{
+    public string NumeroPedido;
+    public string NomeCliente;
+    public string Status;
+
+    public Pedido(string numeroPedido, string nomeCliente, string status)
+    {
+        NumeroPedido = numeroPedido;
+        NomeCliente = nomeCliente;
+        Status = status;
+    }
+
+    public string AtualizarStatus(string novoStatus)
+    {
+        return Status = novoStatus;
+    }
+
+    public string ExibirPedido()
+    {
+        return $"Pedido nº: {NumeroPedido} \nCliente: {NomeCliente} \nStatus: {Status}";
+    }
+}
---
+++ SistemaDePedidosLojaVirtual/Program.cs
@@ -0,0 +1,7 @@
+﻿Pedido pedido01 = new Pedido("001","Maria","Pendente");
+Console.WriteLine(pedido01.ExibirPedido());
+
+Console.WriteLine("");
+
+pedido01.AtualizarStatus("Enviado");
+Console.WriteLine(pedido01.ExibirPedido());
